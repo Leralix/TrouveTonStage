93d06f712b3bb9dc9f4bcecd338c55133492ff95
Leralix <78090544+Leralix@users.noreply.github.com>
2023-02-02 16:53:50 +0100

    c bugé

--- Le site/DisplayElasticSearch.py
+++ Le site/DisplayElasticSearch.py
@@ -1,15 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 from elasticsearch import Elasticsearch
 
 app = Flask(__name__)
-
 
 es_client = Elasticsearch(hosts=["http://localhost:9200"])
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('main.html')
 
+<<<<<<< Updated upstream
+=======
+@app.route('/search_results', methods=['GET', 'POST'])
+def search_request():
+    print("test")
+    search_term = request.form["NameInput"]
+    print(search_term)
+
+    body = {
+        "query": {
+            "bool": {
+                "must": [
+                    {
+                        "match": {
+                            "Titre": search_term
+                        }
+                    }
+                ]
+            }
+        }
+    }
+    res = es_client.search(
+        index="job_offer",
+        size=20,
+        body=body)
+
+    return render_template('search_results.html', res=res)
+
+>>>>>>> Stashed changes
 
 
 if __name__ == '__main__':
---
+++ Le site/DisplayElasticSearchOld.py
@@ -0,0 +1,92 @@
+from flask import Flask, render_template, request
+from elasticsearch import Elasticsearch
+
+app = Flask(__name__)
+
+es_client = Elasticsearch(hosts=["http://localhost:9200"])
+
+@app.route('/')
+def home():
+    return render_template('search.html')
+
+
+def checkbox_button():
+    s=""
+    if request.form.get('bac+3'):
+        s+="bac+3 "
+    if request.form.get('bac+4'):
+        s+="bac+4 "
+    if request.form.get('bac+5'):
+        s+="bac+5 "
+    return s.strip()
+
+
+@app.route('/search/results', methods=['GET', 'POST'])
+def search_request():
+    search_term = request.files["ID"]
+
+    search_checkbox = checkbox_button()
+
+    if search_checkbox == "":
+        body = {
+              "query": {
+    "bool": {
+      "must": [
+        {
+          "match": {
+            "Titre": search_term
+          }
+        }
+
+      ]
+    }
+  }
+        }
+    else:
+        body = {
+    "query": {
+    "bool": {
+      "must": [
+        {
+          "match": {
+            "BacFormat": {
+              "query" : search_checkbox,
+              "operator":"and"
+                          }
+          }
+        },
+{
+          "match": {
+            "Titre": search_term
+          }
+        }
+      ]
+    }
+  }
+}
+
+
+
+    res = es_client.search(
+        index="job_offer",
+        size=20,
+        body = body)
+
+    return render_template('results.html', res=res )
+
+if __name__ == '__main__':
+    app.secret_key = 'mysecret'
+    app.run(host='0.0.0.0', port=5000)
+
+
+"""body={
+            "query": {
+                "multi_match" : {
+                    "query": search_term,
+                    "fields": [
+                        "Titre",
+                        "BacFormat"
+                    ]
+                }
+            }
+        })"""
