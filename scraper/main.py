13c4e1b9a3bb8cf75ca413e30bff33f18827d684
Gabe-MGNT <107551867+Gabe-MGNT@users.noreply.github.com>
2023-02-10 10:44:34 +0100

    add

--- scraper/main.py
+++ scraper/main.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import time
-from indeed import IndeedScraper
+from indeed.IndeedScraper import IndeedScraper
 from wttj.WTTJScraper import WTTJScraper
 from threading import Thread
+import os.path
 
 from Data_Cleaner import DataCleaner
 
@@ -42,7 +43,14 @@ time.sleep(3)
 df1 = pd.read_csv("./data/INDEED.csv")
 df2 = pd.read_csv("./data/WTTJ.csv")
 df3 = pd.concat([df1, df2], axis=0, ignore_index=True)
-df3.to_csv('./data/Ind_Wttj.csv', index=False)
+
+
+if os.path.isfile('./data/Ind_Wttj.csv')==True :
+    df4 = pd.read_csv('./data/Ind_Wttj.csv',index=False)
+    df_final = pd.concat([df4,df3],axis=0,ignore_index=True)
+    df_final.to_csv('./data/Ind_Wttj.csv',index=False)
+else :
+    df3.to_csv('./data/Ind_Wttj.csv', index=False)
 
 # Clean csv and export the result
 dc = DataCleaner('./data/Ind_Wttj.csv')
